2c3e1736d757f04bb8a2c55055236348c2349b60
lijian <lijianlong1314@outlook.com>
2025-05-29 14:33:04 +0800

    初始化各个子模块，并且配置好了父pom文件

---
+++ common/src/main/java/com/lijian/App.java
@@ -0,0 +1,13 @@
+package com.lijian;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
